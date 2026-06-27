3a88373ed8fbf5a4b85664b61e81e950e458dae4
Oleg Kovalenko <monstrenyatko@gmail.com>
2026-06-27 02:36:40 -0700

    Do not fail if route is already exists

--- routing.sh
+++ routing.sh
@@ -8,5 +8,8 @@ set -e
 
 if [ -n "$NET_LOCAL" ]; then
   docker_network_gw="$(ip route | awk '/default/{print $3}')"
-  ip route add $NET_LOCAL via $docker_network_gw dev eth0
+  # Check if the route already exists before attempting to add it
+  if ! ip route show "${NET_LOCAL}" >/dev/null 2>&1; then
+    ip route add "${NET_LOCAL}" via "${docker_network_gw}" dev eth0
+  fi
 fi
--- routing6.sh
+++ routing6.sh
@@ -8,5 +8,8 @@ set -e
 
 if [ -n "$NET6_LOCAL" ]; then
   docker_network_gw="$(ip -6 route | awk '/default/{print $3}')"
-  ip route add $NET6_LOCAL via $docker_network_gw dev eth0
+  # Check if the route already exists before attempting to add it
+  if ! ip -6 route show "${NET6_LOCAL}" >/dev/null 2>&1; then
+    ip -6 route add "${NET6_LOCAL}" via "${docker_network_gw6}" dev eth0
+  fi
 fi
